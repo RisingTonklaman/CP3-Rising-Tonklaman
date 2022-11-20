0d724d96e23289cee1c6e084b97cc2efec88bc68
RisingTonklaman <21398@dusitaram.ac.th>
2022-11-20 13:46:40 +0700

    Update defineMACADRES.ino

--- ALDUINO/defineMACADRES/defineMACADRES.ino
+++ ALDUINO/defineMACADRES/defineMACADRES.ino
@@ -4,14 +4,14 @@
   #include <ESP8266WiFi.h>
 #endif
 
-uint8_t broadcastAddress1=WiFi.macAddress();
+
 void setup(){
   Serial.begin(115200);
   Serial.println();
   Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
+ 
 }
  
 void loop(){
-Serial.println(broadcastAddress1[]);
+ Serial.println(WiFi.macAddress());
 }
