845e41d68f1dc2e803ef6e270ca96e51b3a1f62f
RisingTonklaman <21398@dusitaram.ac.th>
2022-11-21 14:01:08 +0700

    add new

---
+++ Colection/main.py
@@ -0,0 +1,17 @@
+menuList=[]
+priseList=[]
+while True:
+  menuName = input('Plese Enter Menu :')
+  if(menuName.lower() == 'exit'):
+    break
+  else:
+    priseName = int(input('Enter Your Prise :'))
+    menuList.append(menuName)
+    priseList.append(priseName)
+i=1
+total=0
+while i <= len(menuList) :
+  print(menuList[i-1],'     ',priseList[i-1])
+  total+=priseList[i-1]
+  i+=1
+print('totalprise =',total)
